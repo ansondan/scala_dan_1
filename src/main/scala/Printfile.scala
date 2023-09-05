0cdf9d3365ca922c03230a3a96dfd98e846763e2
Consultant <adanielansong@gmail.com>
2023-09-05 19:56:39 -0400

    5th commit

---
+++ src/main/scala/Printfile.scala
@@ -0,0 +1,8 @@
+object Printfile {
+  def main(args: Array[String]): Unit = {
+    val firstName = "John"
+    val mi = 'C'
+    val lastName = "Doe"
+    println(s"Name: $firstName $mi $lastName")
+  }
+}
