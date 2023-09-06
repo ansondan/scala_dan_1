a62c518f2874556c62b0445c5793f433ceeaa907
Consultant <adanielansong@gmail.com>
2023-09-05 20:31:54 -0400

    8th Commit

---
+++ src/main/scala/Concatenatefile.scala
@@ -0,0 +1,6 @@
+object Concatenatefile {
+  def main(args: Array[String]): Unit = {
+    def concatenate(s1: String, s2: String): String = s1 + s2
+      println(concatenate("learn", "scala"))
+  }
+}
